590ef1a9946b2395200011f26b89599643520711
Michmol13 <159731985+Michmol13@users.noreply.github.com>
2025-03-27 11:31:42 -0600

    Registro de Hijos

---
+++ js/registroHijos.js
@@ -0,0 +1,59 @@
+const inputnombreHijo = document.getElementById("txtnombreHijo");
+const inputnivelEducativo = document.getElementById("txtnivelEducativo");
+const inputannoLectivo = document.getElementById("txtannoLectivo");
+const btnGuardar = document.querySelector("#btnGuardar");
+
+const inputsRequeridos = document.querySelectorAll('input[required]');
+
+function validar() {
+    let error = false;
+    for (let i = 0; i < inputsRequeridos.length; i++){
+        if (inputsRequeridos[i].value == ""){
+            inputsRequeridos[i].classList.add('error');
+            error = true;
+        } else {
+            inputsRequeridos[i].classList.remove('error');
+        }
+    }
+
+    if(error == false){
+        registrarHijo();
+    } 
+}
+
+function registrarHijo(){
+    const datosHijos = {
+        nombreHijo: inputnombreHijo.value,
+        nivelEducativo: inputnivelEducativo.value,
+        annoLectivo: inputannoLectivo.value,
+    };
+    fetch("http://localhost:3000/registrohijos", {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(datosHijos)
+    }).then(response => {
+        if (response.ok) {
+            Swal.fire({
+                icon: "success",
+                title: "Hijo registrado",
+                text: "Su hijo ha sido registrado con éxito"
+            });
+        } else {
+            Swal.fire({
+                icon: "error",
+                title: "Error",
+                text: "No se pudo registrar a su hijo"
+            });
+        }
+    })
+    .catch(error => {
+        console.log(error);
+    });
+    
+}
+
+
+
+btnGuardar.addEventListener('click', validar);
