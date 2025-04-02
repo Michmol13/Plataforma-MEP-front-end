a29d334c16ef39438a198ba4dd123b0e2ae9e6a6
Brandon1606 <bhernandezs@ucenfotec.ac.cr>
2025-04-01 20:38:46 -0600

    js Brandon

---
+++ js/registroUsuarios.js
@@ -0,0 +1,64 @@
+const inputnombreCompleto = document.getElementById("txtnombreCompleto");
+const inputcedula = document.getElementById("txtcedula");
+const inputcorreoElectronico = document.getElementById("txtcorreoElectronico");
+const inputcontrasenal = document.getElementById("txtcontrasenna");
+const inputconfirmarContrasena = document.getElementById("txtconfirmarContrasena");
+const inputrol= document.getElementById("txtrol");
+const inputestadoCuenta = document.getElementById("txtestadoCuenta");
+const btnGuardar = document.querySelector("#btnGuardar");
+
+const inputsRequeridos = document.querySelectorAll('input[required], textarea[required], select[required]');
+
+
+function validar() {
+    let error = false;
+    for (let i = 0; i < inputsRequeridos.length; i++){
+        if (inputsRequeridos[i].value == ""){
+            inputsRequeridos[i].classList.add('error');
+            error = true;
+        } else {
+            inputsRequeridos[i].classList.remove('error');
+        }
+    }
+
+    if(error == false){
+        registrarUsuario();
+    } 
+}
+
+function registrarUsuario() {
+    const datosUsuario = {
+        nombreNivel: inputnombreNivel.value,
+        descripcion: inputdescripcionNivel.value,
+        estado: inputestadoNivel.value,
+    };
+
+    fetch("http://localhost:3000/registroNivelesEducativos", {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(datosNivel)
+    })
+    .then(response => {
+        if (response.ok) {
+            Swal.fire({
+                icon: "success",
+                title: "Nivel Educativo registrado",
+                text: "El nivel educativo ha sido registrado con éxito"
+            });
+        } else {
+            Swal.fire({
+                icon: "error",
+                title: "Error",
+                text: "No se pudo registrar el nivel educativo"
+            });
+        }
+    })
+    .catch(error => {
+        console.log(error);
+    });
+}
+
+// Agregar evento al botón "Guardar"
+btnGuardar.addEventListener('click', validar);
