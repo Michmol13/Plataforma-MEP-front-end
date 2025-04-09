4dab74c7de1a5f7f375e1418837f535fcadeb259
Michmol13 <159731985+Michmol13@users.noreply.github.com>
2025-04-08 20:39:21 -0600

    consulta

---
+++ js/registroUsuarios-consultar.js
@@ -0,0 +1,29 @@
+const tablaUsuarios = document.getElementById('tabla-registroUsuarios').querySelector('tbody');
+
+async function cargarTabla() {
+    fetch('http://localhost:3000/usuarios',{
+        method: 'GET'
+        ,headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        tablaUsuarios.innerHTML = '';
+        data.forEach(Usuarios => {
+            const fila = document.createElement('tr');
+
+            fila.innerHTML = `
+                <td>${Usuarios.nombreCompleto}</td>
+                <td>${Usuarios.cedula}</td>
+                <td>${Usuarios.correoElectronico}</td>
+                <td>${Usuarios.rol}</td>
+                <td>${Usuarios.estadoCuenta ? 'Activo' : 'Inactivo'}</td>
+            `;
+            tablaUsuarios.appendChild(fila);
+        });
+    })
+
+}
+
+cargarTabla();
